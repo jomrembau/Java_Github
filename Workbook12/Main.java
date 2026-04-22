c74be6e54320da52de539256c2cf88eaddac3477
jomrembau <jomir.bautista@gmail.com>
2026-04-22 12:53:28 +0200

    Edited main

---
+++ Workbook12/Main.java
@@ -0,0 +1,12 @@
+package Workbook12;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Cylinder cylinder = new Cylinder(5, 29);
+        cylinder.setHeight(42);
+
+        Sphere sphere = new Sphere(7);
+        sphere.setRadius(9);
+    }
+}
