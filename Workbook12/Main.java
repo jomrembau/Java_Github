944cd81c4f9bad97b60054626ec27faf06af19fe
jomrembau <jomir.bautista@gmail.com>
2026-04-22 13:03:22 +0200

    Cylinder surface area

--- Workbook12/Main.java
+++ Workbook12/Main.java
@@ -8,5 +8,14 @@ public class Main {
 
         Sphere sphere = new Sphere(7);
         sphere.setRadius(9);
+
+        System.out.println("\nCylinder Radius: " + cylinder.getRadius());
+        System.out.println("Cylinder Height: " + cylinder.getHeight());
+        System.out.println("Surface Area: " + cylinderVolume(cylinder.getRadius(),cylinder.getHeight()));
+    }
+
+    public static double cylinderVolume(double radius, double height) {
+        double value = Math.PI * (radius * radius) * height;
+        return value;
     }
 }
---
+++ Workbook12/Sphere.java
@@ -0,0 +1,10 @@
+package Workbook12;
+
+public class Sphere extends Shape {
+    
+
+    public Sphere(double radius) {
+        super(radius);
+    }
+    
+}
